dfef33d29bf8f31d25e5ae74cf8fc73157628695
paulodlima <paulodlima2014@gmail.com>
2020-04-14 09:10:33 -0300

    Alteração da senha para preencher os requisitos do frontEnd

--- src/main/java/com/person/api/service/PasswordEncoderTest.java
+++ src/main/java/com/person/api/service/PasswordEncoderTest.java
@@ -6,6 +6,6 @@ public class PasswordEncoderTest {
     public static void main(String[] args) {
               BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-              System.out.println(passwordEncoder.encode("gestor"));
+              System.out.println(passwordEncoder.encode("admin123"));
     }
 }
